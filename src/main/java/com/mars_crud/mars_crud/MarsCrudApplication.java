960a6788a6e373b6db333e557b10706eacd02498
alkminisarigeorgiou <alkminisarigeorgiou@gmail.com>
2025-04-03 14:26:11 +0300

    attempt 500000

---
+++ src/main/java/com/mars_crud/mars_crud/MarsCrudApplication.java
@@ -0,0 +1,13 @@
+package com.mars_crud.mars_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarsCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarsCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mars_crud/mars_crud/MarsCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mars_crud.mars_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarsCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
